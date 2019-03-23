4ea5970688ddb12b2e893eae05ca6fead1f08a73
Cesar Quiroga <cesar.a.quiroga.c@gmail.com>
2019-03-23 16:42:24 -0400

    login y registro

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -7,17 +7,6 @@ use \App\Estudiante;
 use \App\IniciarSesion;
 use \App\AsignaRol;
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -26,19 +15,8 @@ Route::get('home', function(){
     echo 'Welcome Home';
 });
 
-Route::get('registrarse', 'RegistroEstudianteController@getRegistro');
-Route::post('registrarse', 'RegistroEstudianteController@postRegistro');
+Route::get('registro', 'RegistroEstudianteController@getRegistro');
+Route::post('registro', 'RegistroEstudianteController@postRegistro');
 
 Route::get('login', 'IngresoUsuarioController@getLogin');
 Route::post('login', 'IngresoUsuarioController@postLogin');
-
-/*
-// Authentication routes...
-Route::get('auth/login', 'Auth\AuthController@getLogin');
-Route::post('auth/login', 'Auth\AuthController@postLogin');
-Route::get('auth/logout', 'Auth\AuthController@getLogout');
-
-// Registration routes...
-Route::get('auth/register', 'Auth\AuthController@getRegister');
-Route::post('auth/register', 'Auth\AuthController@postRegister');
-*/
