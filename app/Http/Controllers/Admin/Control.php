cc452846e19e0ca5d3112631438ed118de55bae6
williamVMA <williamVMA>
2019-03-24 17:21:15 -0400

    nada relevante

--- app/Http/Controllers/Admin/Control.php
+++ app/Http/Controllers/Admin/Control.php
@@ -1,44 +1,16 @@
 <?php
-<<<<<<< HEAD
-
-=======
->>>>>>> master
 namespace App\Http\Controllers\Admin;
 
 use App\Usuario;
 use App\AsignaRol;
-<<<<<<< HEAD
-use App\Admin;
-use App\Http\Controllers\Controller;
-=======
 use App\Estudiante;
 use App\Classes\Rol;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\Admin\Base;
->>>>>>> master
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
-<<<<<<< HEAD
-class Control extends Controller
-{
-    public function getEdit()
-    {
-        return view('welcome');
-    }
-
-    public function postEdit()
-    {
-        return view('welcome');
-    }
-
-    public function getVista()
-    {
-        return view('admin.vistaInicial');
-    }
-
-=======
 class Control extends Base
 {
     public function getVista(Request $request)
@@ -68,5 +40,4 @@ class Control extends Base
         
         return redirect('login');
     }
->>>>>>> master
 }
