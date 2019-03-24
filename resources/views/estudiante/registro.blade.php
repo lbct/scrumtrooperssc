24b14e15fd9b24225c50218ea1cfd1cadc1ff50f
Alex <alex.cachnd@gmail.com>
2019-03-24 19:03:12 -0400

    Calendario de Registro Arreglado

--- resources/views/estudiante/registro.blade.php
+++ resources/views/estudiante/registro.blade.php
@@ -2,6 +2,10 @@
 <script src="{{asset('jquery/jquery.min.js')}}"></script>
 <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
 
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="https://unpkg.com/gijgo@1.9.13/js/gijgo.min.js" type="text/javascript"></script>
+<link href="https://unpkg.com/gijgo@1.9.13/css/gijgo.min.css" rel="stylesheet" type="text/css" />
+
 @extends('inicio')
 
 @section('main-content')
@@ -55,9 +59,16 @@
                                             <option value="F">Femenino</option>
                                     </select>
                                 </div>
-                                <div class="form-group column2">
-                                        <input type="date" id="fecha_nacimiento" name="fecha_nacimiento">
-                                    </div>
+                                <div class="form-group column2 row justify-content-center">
+                                
+                                    <input id="datepicker" name="fecha_nacimiento" placeholder="Fecha de Nacimiento"/>
+                                    <script>
+                                        $('#datepicker').datepicker({
+                                            showOtherMonths: true
+                                        });
+                                    </script>
+                                
+                                
                                 </div>
                         </div>
 
