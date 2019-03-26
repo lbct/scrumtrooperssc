ead0e3840be57ff9f98d60c1016514b7411ebb1a
Alex <alex.cachnd@gmail.com>
2019-03-25 20:46:02 -0400

    Ocultar Spinners de Formularios

--- resources/views/estudiante/editar.blade.php
+++ resources/views/estudiante/editar.blade.php
@@ -6,6 +6,7 @@
 @section('contenido')
 <form method="POST" action="/estudiante/editar">
     {!! csrf_field() !!}
+    <link href="{{asset('/css/campos.css')}}" rel="stylesheet" id="bootstrap-css">
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     <script src="https://unpkg.com/gijgo@1.9.13/js/gijgo.min.js" type="text/javascript"></script>
     <link href="https://unpkg.com/gijgo@1.9.13/css/gijgo.min.css" rel="stylesheet" type="text/css" />
--- resources/views/formulario.blade.php
+++ resources/views/formulario.blade.php
@@ -2,6 +2,7 @@
       <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
       <script src="https://unpkg.com/gijgo@1.9.13/js/gijgo.min.js" type="text/javascript"></script>
       <link href="https://unpkg.com/gijgo@1.9.13/css/gijgo.min.css" rel="stylesheet" type="text/css" />
+      <link href="{{asset('/css/campos.css')}}" rel="stylesheet" id="bootstrap-css">
       <div class="container col-md-5 col-md-offset-5 ">
           <div class="row">
               <div class="panel panel-default">
