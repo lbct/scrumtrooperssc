00eec7931955b44c6939b2a538833f9ee82f7e1a
Alex <alex.cachnd@gmail.com>
2019-04-01 13:49:31 -0400

    Vista Inscripcion

--- resources/views/estudiante/inscripcion.blade.php
+++ resources/views/estudiante/inscripcion.blade.php
@@ -6,54 +6,95 @@
 <!-- AQUI EL CONTENIDO :V-->
 
 <link href="{{asset('css/bs-stepper.min.css')}}" rel="stylesheet">
+@include('alertas')
+@include('errores')
+{!! csrf_field() !!}
+
 
 <div class="container flex-grow-1 flex-shrink-0 py-5">
-        <div class="mb-5 p-4 bg-white shadow-sm">
-          <h3>Linear stepper</h3>
-          <div id="stepper1" class="bs-stepper">
+        <div class="mb-5 p-4">
+          <h3>Formulario de Inscripcion</h3>
+          <div id="stepper" class="bs-stepper">
             <div class="bs-stepper-header" role="tablist">
               <div class="step" data-target="#test-l-1">
-                <button type="button" class="step-trigger" role="tab" id="stepper1trigger1" aria-controls="test-l-1">
+                <button type="button" class="step-trigger" role="tab" id="steppertrigger1" aria-controls="test-l-1">
                   <span class="bs-stepper-circle">1</span>
-                  <span class="bs-stepper-label">Email</span>
+                  <span class="bs-stepper-label">Materia</span>
                 </button>
               </div>
               <div class="bs-stepper-line"></div>
               <div class="step" data-target="#test-l-2">
-                <button type="button" class="step-trigger" role="tab" id="stepper1trigger2" aria-controls="test-l-2">
+                <button type="button" class="step-trigger" role="tab" id="steppertrigger2" aria-controls="test-l-2">
                   <span class="bs-stepper-circle">2</span>
-                  <span class="bs-stepper-label">Password</span>
+                  <span class="bs-stepper-label">Docente</span>
                 </button>
               </div>
               <div class="bs-stepper-line"></div>
               <div class="step" data-target="#test-l-3">
-                <button type="button" class="step-trigger" role="tab" id="stepper1trigger3" aria-controls="test-l-3">
+                <button type="button" class="step-trigger" role="tab" id="steppertrigger3" aria-controls="test-l-3">
                   <span class="bs-stepper-circle">3</span>
-                  <span class="bs-stepper-label">Validate</span>
+                  <span class="bs-stepper-label">Horario</span>
+                </button>
+              </div>
+              <div class="bs-stepper-line"></div>
+              <div class="step" data-target="#test-l-4">
+                <button type="button" class="step-trigger" role="tab" id="steppertrigger4" aria-controls="test-l-4">
+                  <span class="bs-stepper-circle">4</span>
+                  <span class="bs-stepper-label">Verificacion</span>
                 </button>
               </div>
             </div>
             <div class="bs-stepper-content">
               <form method="POST" action="/estudiante/inscripcion">
-                {!! csrf_field() !!}
-                <div id="test-l-1" role="tabpanel" class="bs-stepper-pane" aria-labelledby="stepper1trigger1">
+                <div id="test-l-1" role="tabpanel" class="bs-stepper-pane" aria-labelledby="steppertrigger1">
                   <div class="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                    <label for="materia">Seleciona la Materia</label>
+                    <select class="form-control" id="materia" value="{{ old('materia') }}">
+                      <option value="1">Intro</option>
+                      <option value="2">Elementos</option>
+                      <option value="3">Taller de Progra</option>
+                      <option value="4">TIS :(</option>
+                    </select>
                   </div>
-                  <button class="btn btn-primary" onclick="stepper1.next()" type="button">Next</button>
+                  <button class="btn btn-primary" onclick="stepper.next()" type="button">Next</button>
                 </div>
-                <div id="test-l-2" role="tabpanel" class="bs-stepper-pane" aria-labelledby="stepper1trigger2">
+                <div id="test-l-2" role="tabpanel" class="bs-stepper-pane" aria-labelledby="steppertrigger2">
                   <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    <label for="docente">Seleciona tu Docente</label>
+                    <select class="form-control" id="docente" value="{{ old('docente') }}">
+                      <option value="1">Leticia</option>
+                      <option value="2">Vladimir</option>
+                      <option value="3">Flores</option>
+                      <option value="4">TIS :(</option>
+                    </select>
                   </div>
-                  <button class="btn btn-primary" onclick="stepper1.previous()" type="button">Previous</button>
-                  <button class="btn btn-primary" onclick="stepper1.next()" type="button">Next</button>
+                  <button class="btn btn-primary" onclick="stepper.previous()" type="button">Previous</button>
+                  <button class="btn btn-primary" onclick="stepper.next()" type="button">Next</button>
                 </div>
-                <div id="test-l-3" role="tabpanel" class="bs-stepper-pane text-center" aria-labelledby="stepper1trigger3">
-                  <button class="btn btn-primary mt-5" onclick="stepper1.previous()" type="button">Previous</button>
-                  <button type="submit" class="btn btn-primary mt-5">Submit</button>
+                <div id="test-l-3" role="tabpanel" class="bs-stepper-pane text-center" aria-labelledby="steppertrigger3">
+                  <div class="form-group">
+                    <label for="horario">Seleciona un Horario</label>
+                    <select class="form-control" id="horario">
+                      <option value="1">Lunes 6'45"</option>
+                      <option value="2">Lunes 6:45</option>
+                      <option value="3">Lunes 00:00</option>
+                      <option value="4">TIS :(</option>
+                    </select>
+                  </div>
+                  <button class="btn btn-primary" onclick="stepper.previous()" type="button">Previous</button>
+                  <button class="btn btn-primary" onclick="stepper.next()" type="button">Next</button>
+                </div>
+                <div id="test-l-4" role="tabpanel" class="bs-stepper-pane text-center" aria-labelledby="steppertrigger4">
+                  <div class="form-group">
+                      <label for="verficar_materia">Materia</label>
+                      <input type="text" class="form-control" id="verificar_materia" value="Materia Selecionada" disabled>
+                      <label for="verficar_docente">Docente</label>
+                      <input type="text" class="form-control" id="verificar_docente" value="Docente Selecionado" disabled>
+                      <label for="verficar_horario">Horario</label>
+                      <input type="text" class="form-control" id="verificar_horario" value="Horario Selecionada" disabled>
+                  </div>
+                  <button class="btn btn-primary mt-5" onclick="stepper.previous()" type="button">Previous</button>
+                  <button type="submit" class="btn btn-primary mt-5">Inscibirse</button>
                 </div>
               </form>
             </div>
@@ -63,21 +104,21 @@
 
 <script src="{{asset('js/bs-stepper.min.js')}}"></script>
 <script>
-      var stepper1Node = document.querySelector('#stepper1')
-      var stepper1 = new Stepper(document.querySelector('#stepper1'))
-      stepper1Node.addEventListener('show.bs-stepper', function (event) {
-        console.warn('show.bs-stepper', event)
+      var stepperNode = document.querySelector('#stepper')
+      var stepper = new Stepper(document.querySelector('#stepper'))
+      stepperNode.addEventListener('show.bs-stepper', function (event) {
+        //console.warn('show.bs-stepper', event)
       })
-      stepper1Node.addEventListener('shown.bs-stepper', function (event) {
-        console.warn('shown.bs-stepper', event)
+      stepperNode.addEventListener('shown.bs-stepper', function (event) {
+        //console.warn('shown.bs-stepper', event)
       })
-      var stepper2 = new Stepper(document.querySelector('#stepper2'), {
+      /*var stepper2 = new Stepper(document.querySelector('#stepper2'), {
           linear: false,
           animation: true
         })
       var stepper3 = new Stepper(document.querySelector('#stepper3'), {
           animation: true
-        })
+        })*/
 </script>
 
 @endsection
