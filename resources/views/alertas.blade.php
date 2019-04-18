d93998562850c5edc520e552e591569231965a9e
wil0512 <30740067+wil0512@users.noreply.github.com>
2019-04-17 23:11:28 -0400

    alv2

--- resources/views/alertas.blade.php
+++
@@ -1,7 +0,0 @@
-@foreach (['danger', 'warning', 'success', 'info'] as $msg)
-@if(Session::has('alert-' . $msg))
-<div class="flash-message">
-   <p class="alert alert-{{ $msg }}">{{ Session::get('alert-' . $msg) }} <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a></p>
-</div>
-@endif
-@endforeach
