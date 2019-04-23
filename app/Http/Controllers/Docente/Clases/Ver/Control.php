6d310a9fe96e2e1f788f70fedf7607d2bd4d501a
Bernardo Caussin <lbct95@gmail.com>
2019-04-23 01:16:34 -0400

    Subir practica y editar practica desde docente corregido, muestra mensajes

--- app/Http/Controllers/Docente/Clases/Ver/Control.php
+++ app/Http/Controllers/Docente/Clases/Ver/Control.php
@@ -6,6 +6,7 @@ use App\Models\Sesion;
 use App\Models\Materia;
 use App\Models\GrupoDocente;
 use App\Models\GrupoADocente;
+use App\Models\GuiaPractica;
 use App\Models\Auxiliar;
 use App\Models\Docente;
 use App\Models\Gestion;
@@ -37,13 +38,6 @@ class Control extends Base
             $docenteId = Docente::where('USUARIO_ID', '=', $usuarioId)->get()->first()->ID;
             $gestiones = Gestion::select()->orderByRaw('ANO_GESTION desc, ID desc')->get();
             
-            $ultimaSemana = Sesion::join('CLASE', 'CLASE.ID', '=', 'SESION.CLASE_ID')
-            ->join('GRUPO_A_DOCENTE', 'GRUPO_A_DOCENTE.ID', '=', 'CLASE.GRUPO_A_DOCENTE_ID')
-            ->where('GRUPO_A_DOCENTE.DOCENTE_ID', '=', $docenteId)
-            ->where('CLASE.GESTION_ID', '=', $id_gestion)
-            ->orderBy('SESION.SEMANA', 'DESC')
-            ->select('SESION.SEMANA')
-            ->first();
 
             $gruposDocente = GrupoDocente::join('GRUPO_A_DOCENTE', 'GRUPO_A_DOCENTE.GRUPO_DOCENTE_ID', '=', 'GRUPO_DOCENTE.ID')
             ->join('MATERIA', 'MATERIA.ID', '=', 'GRUPO_DOCENTE.MATERIA_ID')
@@ -52,36 +46,46 @@ class Control extends Base
             ->select('GRUPO_DOCENTE.ID', 'GRUPO_DOCENTE.DETALLE_GRUPO_DOCENTE', 'MATERIA.NOMBRE_MATERIA', 'MATERIA.CODIGO_MATERIA')
             ->get();
 
-            $ultimaSemanaValor = 1;
-            if($ultimaSemana != null)
-                $ultimaSemanaValor = $ultimaSemana->SEMANA + 1;
-
             return view('docente.ver.clases')
                 ->with('id_gestion', $id_gestion)
                 ->with('gestiones', $gestiones)
-                ->with('materias', $gruposDocente)
-                ->with('ultima_semana', $ultimaSemanaValor);
+                ->with('materias', $gruposDocente);
         }
         return redirect('login');
     }
 
     public function postClases(Request $request){
         $grupo_docente_id = $request->grupo_docente_id;
-        $semana_valor = $request->semana_valor;
+        //$semana_valor = $request->semana_valor;
         $gestion_id = $request->gestion_id;
         $usuarioId = $request->cookie('USUARIO_ID');
         $docenteId = Docente::where('USUARIO_ID', '=', $usuarioId)->get()->first()->ID;
         $grupoADoc = GrupoADocente::whereRaw('GRUPO_A_DOCENTE.DOCENTE_ID='.$docenteId.' AND GRUPO_A_DOCENTE.GRUPO_DOCENTE_ID='.$grupo_docente_id)->get()->first();
-        //AQUI REVISAR/////////////////////////////
-        $clase_id = $grupoADoc->clase->first()->ID;
-        ///////////////////////////////////////////
-        $nombreArchivo = $request->cookie('USUARIO_ID').'_'.$clase_id.'_'.$semana_valor.'_'.$gestion_id;
         
+        $guiaPracticaResult = GuiaPractica::orderBy('ID', 'DESC')->first();
+        $guiaPracticaId = 1;
+        if($guiaPracticaResult !== null)
+            $guiaPracticaId = $guiaPracticaResult->ID + 1;
+
+        $nombreArchivo = $request->cookie('USUARIO_ID').'_'.$grupoADoc->ID.'_'.$guiaPracticaId.'_'.$gestion_id;
+        
+        $ultimaSemana = Sesion::join('CLASE', 'CLASE.ID', '=', 'SESION.CLASE_ID')
+        ->join('GRUPO_A_DOCENTE', 'GRUPO_A_DOCENTE.ID', '=', 'CLASE.GRUPO_A_DOCENTE_ID')
+        ->where('GRUPO_A_DOCENTE.ID', '=', $grupoADoc->ID)
+        ->orderBy('SESION.SEMANA', 'DESC')
+        ->select('SESION.SEMANA')
+        ->get()->first();
+
+        $ultimaSemanaValor = 1;
+            if($ultimaSemana != null)
+                $ultimaSemanaValor = $ultimaSemana->SEMANA + 1;
+
         return view('docente.subir.practica')
-        ->with('nombre_archivo', $nombreArchivo)
-        ->with('clase_id', $clase_id)
-        ->with('semana_valor', $semana_valor)
+        ->with('grupo_a_docente_id', $grupoADoc->ID)
+        ->with('semana_valor', $ultimaSemanaValor)
         ->with('auxiliar_id', null)
-        ->with('gestion_id', $gestion_id);
+        ->with('gestion_id', $gestion_id)
+        ->with('docente_id', $docenteId)
+        ->with('nombre_archivo', $nombreArchivo);
     }
 }
