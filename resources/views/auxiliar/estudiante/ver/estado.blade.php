90e22b60b57104b3b741e08cf0c3471061063fb7
Bernardo Caussin <lbct95@gmail.com>
2019-05-20 13:15:17 -0400

    Mejor apariencia en Comentar archivo estudiante

--- resources/views/auxiliar/estudiante/ver/estado.blade.php
+++ resources/views/auxiliar/estudiante/ver/estado.blade.php
@@ -9,56 +9,50 @@
 </div>
 <div>
 </div>
-<br>
 @if($sesion_estudiante != null)
-<link href="{{asset('/css/form.css')}}" rel="stylesheet" id="bootstrap-css">
+<link href="{{asset('/css/campos.css')}}" rel="stylesheet" id="bootstrap-css">
 <form id="form_comentario" method="POST" action="/auxiliar/clases/estudiante/practica">
     {!! csrf_field() !!}
-    <div class="wrapper fadeInDown">
-        <div id="formContent">
-            <div>
-                <div class="col-xs-12 col-sm-offset-2 col-md-offset-3">
-                    <br><br>
-                    <h3>Comentar práctica 
-                        @if($practica->EN_LABORATORIO == 1)
-                            de laboratorio
-                        @else
-                            externa
-                        @endif
-                    </h3>
-                    <br>
-                    <center>
-                        <input type="button" value="{{'Descargar '.$practica->ARCHIVO}}"/>
-                    </center>
-                    <center>
-                        <input type="text" name="comentario" id="comentario" class="form-control" placeholder="Comentario" tabindex="1" value="{{ $sesion_estudiante->COMENTARIO_AUXILIAR }}">
-                    </center>
-                    <br>
-                    <div>
-                        <div class="form-group row justify-content-center">
-                            <div class="column2"><input type="button" onclick="submitComentario();" value="Guardar" tabindex="7" style="margin:10px"></div>
-                            <div class="column"><input type="button" onclick="volver();" value="Cancelar" tabindex="7" style="margin:10px">
-                                <script>
-                                        function volver() {
-                                            window.location = "/auxiliar/clases";
-                                        }
-                                        function submitComentario(){
-                                            var form = document.getElementById("form_comentario");
-                                            var hiddenField = document.createElement("input");
-                                            hiddenField.setAttribute("type", "hidden");
-                                            hiddenField.setAttribute("name", 'sesion_estudiante_id');
-                                            hiddenField.setAttribute("value", {{$sesion_estudiante->ID}});
-                                            form.appendChild(hiddenField);
-                                            form.submit();
-                                        }
-                                </script>
-                            </div>
-                        </div>
-                    </div>
-                    <br>
+    <div class="container col-md-5 col-md-offset-4">
+        <br>
+        <p>Comentar práctica 
+            @if($practica->EN_LABORATORIO == 1)
+                de laboratorio
+            @else
+                externa
+            @endif
+        </p>
+        <br>
+        <center>
+            <input class="btn btn-primary" type="button" value="{{'Descargar '.$practica->ARCHIVO}}"/>
+        </center>
+        <br>
+        <center>
+            <textarea type="text" rows="5" name="comentario" id="comentario" class="form-control" style="resize: none" tabindex="1">{{ $sesion_estudiante->COMENTARIO_AUXILIAR }}</textarea>
+        </center>
+        <br>
+        <div>
+            <div class="form-group row justify-content-center">
+                <div class="column2"><input type="button" class="btn btn-primary" onclick="submitComentario();" value="Guardar" tabindex="7" style="margin:10px"></div>
+                <div class="column"><input type="button" class="btn btn-danger" onclick="volver();" value="Cancelar" tabindex="7" style="margin:10px">
+                    <script>
+                            function volver() {
+                                window.location = "/auxiliar/clases";
+                            }
+                            function submitComentario(){
+                                var form = document.getElementById("form_comentario");
+                                var hiddenField = document.createElement("input");
+                                hiddenField.setAttribute("type", "hidden");
+                                hiddenField.setAttribute("name", 'sesion_estudiante_id');
+                                hiddenField.setAttribute("value", {{$sesion_estudiante->ID}});
+                                form.appendChild(hiddenField);
+                                form.submit();
+                            }
+                    </script>
                 </div>
             </div>
         </div>
+        <br>
     </div>
 </form>
 @else
