640b7e153eb7f0c1dee546321ff3b812bc869d7d
Cesar Quiroga <cesar.a.quiroga.c@gmail.com>
2019-06-26 03:19:13 -0400

    seeders

--- database/seeds/SesionSeeder.php
+++ database/seeds/SesionSeeder.php
@@ -9,271 +9,271 @@ class SesionSeeder extends Seeder
     {
         Sesion::create([
         	'CLASE_ID' 		    =>	1,
-        	'AUXILIAR_ID'		=>	4,
+        	
         	'GUIA_PRACTICA_ID'	=>	1,
             'SEMANA'            =>	1,
         ]);
         Sesion::create([
             'CLASE_ID' 		    =>	2,
-            'AUXILIAR_ID'		=>	4,
+            
             'GUIA_PRACTICA_ID'	=>	1,
             'SEMANA'            =>	1,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	3,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	1,
                     'SEMANA'            =>	1,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	1,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	2,
                     'SEMANA'            =>	2,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	2,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	2,
                     'SEMANA'            =>	2,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	3,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	2,
                     'SEMANA'            =>	2,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	1,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	3,
                     'SEMANA'            =>	3,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	2,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	3,
                     'SEMANA'            =>	3,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	3,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	3,
                     'SEMANA'            =>	3,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	1,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	4,
                     'SEMANA'            =>	4,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	2,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	4,
                     'SEMANA'            =>	4,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	3,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	4,
                     'SEMANA'            =>	4,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	1,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	5,
                     'SEMANA'            =>	5,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	2,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	5,
                     'SEMANA'            =>	5,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	3,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	5,
                     'SEMANA'            =>	5,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	1,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	6,
                     'SEMANA'            =>	6,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	2,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	6,
                     'SEMANA'            =>	6,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	3,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	6,
                     'SEMANA'            =>	6,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	1,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	7,
                     'SEMANA'            =>	7,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	2,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	7,
                     'SEMANA'            =>	7,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	3,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	7,
                     'SEMANA'            =>	7,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	1,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	8,
                     'SEMANA'            =>	8,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	2,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	8,
                     'SEMANA'            =>	8,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	3,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	8,
                     'SEMANA'            =>	8,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	1,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	9,
                     'SEMANA'            =>	9,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	2,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	9,
                     'SEMANA'            =>	9,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	3,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	9,
                     'SEMANA'            =>	9,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	1,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	10,
                     'SEMANA'            =>	10,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	2,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	10,
                     'SEMANA'            =>	10,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	3,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	10,
                     'SEMANA'            =>	10,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	1,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	11,
                     'SEMANA'            =>	11,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	2,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	11,
                     'SEMANA'            =>	11,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	3,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	11,
                     'SEMANA'            =>	11,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	1,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	12,
                     'SEMANA'            =>	12,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	2,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	12,
                     'SEMANA'            =>	12,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	3,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	12,
                     'SEMANA'            =>	12,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	1,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	13,
                     'SEMANA'            =>	13,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	2,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	13,
                     'SEMANA'            =>	13,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	3,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	13,
                     'SEMANA'            =>	13,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	1,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	14,
                     'SEMANA'            =>	14,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	2,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	14,
                     'SEMANA'            =>	14,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	3,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	14,
                     'SEMANA'            =>	14,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	1,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	15,
                     'SEMANA'            =>	15,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	2,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	15,
                     'SEMANA'            =>	15,
         ]);
         Sesion::create([
                     'CLASE_ID' 		    =>	3,
-                    'AUXILIAR_ID'		=>	4,
+                    
                     'GUIA_PRACTICA_ID'	=>	15,
                     'SEMANA'            =>	15,
         ]);
