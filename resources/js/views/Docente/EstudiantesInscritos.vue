40c9648e4b50af9d0bbc93adeb9363598f8828e7
Cesar Quiroga <cesar.a.quiroga.c@gmail.com>
2019-07-30 14:56:50 -0400

    loading view

--- resources/js/views/Docente/EstudiantesInscritos.vue
+++ resources/js/views/Docente/EstudiantesInscritos.vue
@@ -26,54 +26,59 @@
                         </select>
                   </div>
                 </center>
-                <div v-if="estudiantes.length > 0">
-                <div class="text-right">
-                    <h4>Total de estudiantes: {{estudiantes.length}}</h4>
-                    <a :href="'/docente/portafolios/'+materia.id"
-                       target="_blank" class="mb-3 btn btn-primary">
-                        Descargar Portafolios
-                    </a>
-                </div>
-                <div class="text-left">
-                    <div class="col-xs-12 col-lg-4">
-                        <div class="input-group mb-2">
-                            <div class="input-group-prepend">
-                              <div class="input-group-text">
-                                  <i class="fas fa-search"></i>
-                              </div>
+                
+                <div v-if="carga_estudiantes">
+                    <div v-if="estudiantes.length > 0">
+                        <div class="text-right">
+                            <h4>Total de estudiantes: {{estudiantes.length}}</h4>
+                            <a :href="'/docente/portafolios/'+materia.id"
+                               target="_blank" class="mb-3 btn btn-primary">
+                                Descargar Portafolios
+                            </a>
+                        </div>
+                        <div class="text-left">
+                            <div class="col-xs-12 col-lg-4">
+                                <div class="input-group mb-2">
+                                    <div class="input-group-prepend">
+                                      <div class="input-group-text">
+                                          <i class="fas fa-search"></i>
+                                      </div>
+                                    </div>
+                                    <input v-model="busqueda" v-on:keyup="filtrar()"
+                                           type="text" class="form-control" placeholder="Nombre del estudiante">
+                                </div>
                             </div>
-                            <input v-model="busqueda" v-on:keyup="filtrar()"
-                                   type="text" class="form-control" placeholder="Nombre del estudiante">
+                            <br>
+                        </div>
+
+                        <div class="table-responsive">
+                            <table class="table table-hover">
+                                <thead class="thead-dark">
+                                    <tr>
+                                        <th scope="col">Código SIS</th>
+                                        <th scope="col">Nombre</th>
+                                        <th scope="col">Apellido</th>
+                                        <th scope="col">Correo</th>
+                                        <th scope="col">Semanas Asistidas</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr v-for="(estudiante, index) in estudiantes_filtrados"
+                                        v-on:click="irAPortafolio(estudiante)"
+                                        class = "clickleable">
+                                        <td>{{estudiante.codigo_sis}}</td>
+                                        <td>{{estudiante.nombre}}</td>
+                                        <td>{{estudiante.apellido}}</td> 
+                                        <td>{{estudiante.correo}}</td>
+                                        <td>{{estudiante.semanas_asistidas}}/{{estudiante.semanas_totales}}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
                     </div>
-                    <br>
+                    <p v-else>No tienes estudiantes inscritos a esta materia</p>
                 </div>
-                <div class="table-responsive">
-                    <table class="table table-hover">
-                        <thead class="thead-dark">
-                            <tr>
-                                <th scope="col">Código SIS</th>
-                                <th scope="col">Nombre</th>
-                                <th scope="col">Apellido</th>
-                                <th scope="col">Correo</th>
-                                <th scope="col">Semanas Asistidas</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr v-for="(estudiante, index) in estudiantes_filtrados"
-                                v-on:click="irAPortafolio(estudiante)"
-                                class = "clickleable">
-                                <td>{{estudiante.codigo_sis}}</td>
-                                <td>{{estudiante.nombre}}</td>
-                                <td>{{estudiante.apellido}}</td> 
-                                <td>{{estudiante.correo}}</td>
-                                <td>{{estudiante.semanas_asistidas}}/{{estudiante.semanas_totales}}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                </div>
-                <p v-else>No tienes estudiantes inscritos a esta materia</p>
+                <div v-else class="mt-5"><circle-spin></circle-spin></div>
             </div>
             <p v-else>No tienes materias disponibles</p>
         </div>
@@ -96,6 +101,8 @@
                 estudiantes: [],
                 estudiantes_filtrados: [],
                 estudiante: {id:'', codigo_sis:'', nombre:'', apellido:'', correo:'', semanas_asistidas:0},
+                
+                carga_estudiantes: false,
             }
         },
     
@@ -141,11 +148,15 @@
             },
             
             cambiarMateria(){
+                this.carga_estudiantes = false;
+                
                 this.axios
                     .get('/docente/estudiantes/'+this.materia.id)
                     .then((response)=>{
                         this.estudiantes = response.data;
                         this.estudiantes_filtrados = response.data;
+                        
+                        this.carga_estudiantes = true;
                     })
                     .catch(function (error) {
                         console.log(error);
